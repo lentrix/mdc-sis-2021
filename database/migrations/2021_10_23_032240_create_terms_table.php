9d0cf46e74e6b8f89c4217ef9ec660d56bb8deec
lentrix <hawkmanlentrix@gmail.com>
2021-10-23 12:15:02 +0800

    terms-periods phase 1

---
+++ database/migrations/2021_10_23_032240_create_terms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTermsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('terms', function (Blueprint $table) {
+            $table->id();
+            $table->string('accronym', 20);
+            $table->string('name');
+            $table->string('type',6);//annual, sem (semestral), tri (trimestral)
+            $table->date('start');
+            $table->date('end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('terms');
+    }
+}
---
+++ database/migrations/2021_10_23_032305_create_periods_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeriodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('periods', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('term_id')->unsigned();
+            $table->string('name');
+            $table->date('start');
+            $table->date('end');
+            $table->timestamps();
+            $table->foreign('term_id')->references('id')->on('terms');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('periods');
+    }
+}
