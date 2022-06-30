73d82fd8fe5eec3060fcba043da5d72f267d68ff
lentrix <hawkmanlentrix@gmail.com>
2022-06-30 13:01:23 +0800

    programs sidebar to head

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -26,7 +26,7 @@
         Records
     </div>
 
-    @if(auth()->user()->is('registrar'))
+    @if(auth()->user()->is('registrar') || auth()->user->is('head'))
     <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseStudents"
             aria-expanded="true" aria-controls="collapseStudents">
