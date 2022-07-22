db727c86d7732fa68e559822edb9f60971b475ad
lentrix <hawkmanlentrix@gmail.com>
2022-07-22 13:55:15 +0800

    dashboad phase 1

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Department;
+use App\Models\Enrol;
+use App\Models\Term;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SiteController extends Controller
 {
@@ -15,6 +19,23 @@ class SiteController extends Controller
     }
 
     public function dashboard() {
-        return view('pages.dashboard');
+        $termIds = Term::getActive()->get('id');
+        $enrols = Enrol::whereIn('term_id', $termIds);
+
+        return view('pages.dashboard',[
+            'studCount' => $enrols->count(),
+            'collegeCount' => $this->deptCount(Department::where('accronym', 'College')->first()),
+            'shsCount' => $this->deptCount(Department::where('accronym', 'SHS')->first()),
+            'jhsCount' => $this->deptCount(Department::where('accronym', 'JHS')->first()),
+            'elemCount' => $this->deptCount(Department::where('accronym', 'Elem')->first()),
+        ]);
+    }
+
+    private function deptCount($dept) {
+        return DB::table('enrols')->whereIn('term_id', Term::getActive()->get('id'))
+            ->join('programs','programs.id','enrols.program_id')
+            ->whereIn('programs.department_id', explode(",", Department::getHierarchyList($dept)))
+            ->select('enrols.id')
+            ->count();
     }
 }
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -5,5 +5,61 @@
 <h1>Dashboard</h1>
 <hr>
 
+<div class="row">
+    <div class="col-md-2 col-sm-4">
+        <div class="card">
+            <div class="card-body">
+                <h1 class="text-center">{{$studCount}}</h1>
+            </div>
+            <div class="card-footer">
+                <h5 class="text-center">Total</h5>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-2 col-sm-4">
+        <div class="card">
+            <div class="card-body">
+                <h1 class="text-center">{{$collegeCount}}</h1>
+            </div>
+            <div class="card-footer">
+                <h5 class="text-center">College</h5>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-2 col-sm-4">
+        <div class="card">
+            <div class="card-body">
+                <h1 class="text-center">{{$shsCount}}</h1>
+            </div>
+            <div class="card-footer">
+                <h5 class="text-center">SHS</h5>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-2 col-sm-4">
+        <div class="card">
+            <div class="card-body">
+                <h1 class="text-center">{{$jhsCount}}</h1>
+            </div>
+            <div class="card-footer">
+                <h5 class="text-center">JHS</h5>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-2 col-sm-4">
+        <div class="card">
+            <div class="card-body">
+                <h1 class="text-center">{{$elemCount}}</h1>
+            </div>
+            <div class="card-footer">
+                <h5 class="text-center">Elem</h5>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
